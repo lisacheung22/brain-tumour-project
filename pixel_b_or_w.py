992abed9b0c63f186e255681847a4fe2f2676e70
emily-ch <124201097+emily-ch@users.noreply.github.com>
2023-02-20 17:25:43 +0000

    Add files via upload

---
+++ pixel_b_or_w.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 19 14:51:09 2023
+
+@author: Emily
+"""
+
+# building a simple model
+
+import torch  # for Pytorch tensors
+from PIL import Image  # python imaging library for loading & manipulating images
+import numpy as np
+
+# Loads the image using .open() from PIL
+# converts it to the RGB colour space
+img = Image.open('image_001.png').convert(
+    'RGB')  # img is now a PIL image object
+
+# converts img to a numpy array then to a PyTorch tensor object
+tensor_img = torch.Tensor(np.array(img))
+
+# Check if the image is all black or all white
+if (tensor_img.min() == 0 and tensor_img.max() == 0):
+    print("The image is all black.")
+elif (tensor_img.min() == 255 and tensor_img.max() == 255):
+    print("The image is all white.")
+else:
+    print("The image is not all black or all white.")
