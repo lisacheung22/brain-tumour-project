49e7d67a148d3c4aec9fb71540513bf71cd02581
emily-ch <124201097+emily-ch@users.noreply.github.com>
2023-02-22 14:14:25 +0000

    Update pixel_b_or_w.py

--- pixel_b_or_w.py
+++ pixel_b_or_w.py
@@ -11,18 +11,20 @@ import torch  # for Pytorch tensors
 from PIL import Image  # python imaging library for loading & manipulating images
 import numpy as np
 
-# Loads the image using .open() from PIL
-# converts it to the RGB colour space
-img = Image.open('image_001.png').convert(
-    'RGB')  # img is now a PIL image object
 
-# converts img to a numpy array then to a PyTorch tensor object
-tensor_img = torch.Tensor(np.array(img))
+def img_to_tensor(img):
+    # Loads the image using .open() from PIL
+    # converts it to the RGB colour space
+    img = Image.open('image_001.png').convert(
+        'RGB')  # img is now a PIL image object
 
-# Check if the image is all black or all white
-if (tensor_img.min() == 0 and tensor_img.max() == 0):
-    print("The image is all black.")
-elif (tensor_img.min() == 255 and tensor_img.max() == 255):
-    print("The image is all white.")
-else:
-    print("The image is not all black or all white.")
+    # converts img to a numpy array then to a PyTorch tensor object
+    tensor_img = torch.Tensor(np.array(img))
+
+    # Check if the image is all black or all white
+    if (tensor_img.min() == 0 and tensor_img.max() == 0):
+        return "The image is all black."
+    elif (tensor_img.min() == 255 and tensor_img.max() == 255):
+        return "The image is all white."
+    else:
+        return "The image is not all black or all white."
