db7f9a2729fd548ee26fb6ce4dde5e22183eb16b
Ke <hunterkezheng@163.com>
2023-03-04 15:17:05 +0000

    reproducibility

--- brain tumour.py
+++ brain tumour.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 %matplotlib inline
 import seaborn as sns
 import random
-random.seed(100)
 
 import torch
 import torchvision
@@ -21,6 +20,12 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Activation
 from tensorflow.keras.layers import Dropout
 print(tf.__version__)
+##############################################################################
+# reproducibility
+random.seed(147)
+np.random.seed(147)
+torch.manual_seed(147)
+tf.random.set_seed(147)
 ##############################################################################
 # run this cell if your jupyter notebook kernel is died
 import os
